7325e75afbf3efe3f84300431b39ac49a1c1b310
sd8917 <sudhanshuraj8917@gmail.com>
2020-02-04 11:48:31 +0530

    initial commit 9

---
+++ Mysite/main/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Tutorial,TutorialCategory,TutorialSeries
+from tinymce.widgets import TinyMCE
+from django.db import models
+
+# Register your models here.
+# overiding the view 
+
+class TutorialAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Title/date", {'fields': ["tutorial_title", "tutorial_published"]}),
+        ("URL", {'fields': ["tutorial_slug"]}),
+        ("Series", {'fields': ["tutorial_series"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 30})},
+        }
+
+
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
+admin.site.register(Tutorial,TutorialAdmin)
